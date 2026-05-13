9ec0fe4e73f2fdc2c90bf5913c34f0ce10166083
Shamsher Khan <shamsherkhan7549@gmail.com>
2026-05-13 09:59:36 -0700

    add: final concept

---
+++ oops/final.php
@@ -0,0 +1,29 @@
+<?php
+// final class Honda{
+
+// }
+
+// class Hero extends Honda{ // cannot extends final class
+
+// }
+
+ class Honda{
+    // final $name= "splendar";// cannot use  final for properties
+
+    final function brand(){
+        echo "Honda brand";
+    }
+}
+
+// class Hero extends Honda{ 
+//  function brand(){              // cannot extends final function
+//         echo "Hero brand";
+//     }
+// }
+
+
+
+
+
+
+?>
